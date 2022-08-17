769f6c33d376a3e4a1fe3a802483c116824e46b1
TheTastyPi <48721067+TheTastyPi@users.noreply.github.com>
2022-08-17 15:14:29 +0800

    design fix

--- tabs.js
+++ tabs.js
@@ -1,9 +1,10 @@
-var currentTab;
+var currentTab = "default";
 function switchTab(id) {
   if (currentTab === id) return;
   if (currentTab) {
     ele(currentTab + "Tab").style.display = "none";
-    ele(currentTab + "Btn").classList.remove("inTab");
+    if (currentTab !== "default")
+      ele(currentTab + "Btn").classList.remove("inTab");
   }
   currentTab = id;
   ele(currentTab + "Tab").style.display = "block";
